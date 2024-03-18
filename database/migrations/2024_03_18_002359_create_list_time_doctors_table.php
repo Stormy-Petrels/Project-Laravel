50a04d58b082d56d2b53faf1e3437106806b5d5c
MaiXuanNghia <nghiamaiquangtri@gmail.com>
2024-03-18 07:47:07 +0700

    create migration for database

---
+++ database/migrations/2024_03_18_002359_create_list_time_doctors_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('list_time_doctor', function (Blueprint $table) {
+            $table->increments('id');
+            $table->time('time')->notNull();
+            $table->decimal('price', 10, 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('list_time_doctor');
+    }
+};
